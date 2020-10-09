48bcda98fed6fd7f441f38efa2d07ec83e2fbc4b
aqil raza <aqilraza49@gmai.com>
2020-10-09 11:19:58 +0530

    Updates

---
+++ precache-manifest.03c7414d5102da9a85148678b4fcf137.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c83c3358ce15af55b628e771a1f02409",
+    "url": "/monster/index.html"
+  },
+  {
+    "revision": "7d47938951da2d251ace",
+    "url": "/monster/static/css/main.fdeb494d.chunk.css"
+  },
+  {
+    "revision": "2272281e4c5004c80e91",
+    "url": "/monster/static/js/2.40bbab4f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster/static/js/2.40bbab4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d47938951da2d251ace",
+    "url": "/monster/static/js/main.e263146c.chunk.js"
+  },
+  {
+    "revision": "5446ca17d6312361b3b2",
+    "url": "/monster/static/js/runtime-main.b7e78b55.js"
+  }
+]);
